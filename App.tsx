2dda72c8a675a93348c6eeb6428ff81264fe8140
danstepanov <danhstepanov@gmail.com>
2022-08-24 12:05:14 -0700

    add react-navigation, initialize project

--- App.tsx
+++ App.tsx
@@ -1,12 +1,29 @@
+import 'react-native-gesture-handler'
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, SafeAreaView } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+type RootStackParamsList = {
+  Home: undefined;
+};
+
+const Stack = createNativeStackNavigator<RootStackParamsList>();
+
+const Home = () => (
+  <SafeAreaView>
+    <StatusBar />
+    <Text>Home</Text>
+  </SafeAreaView>
+)
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+			<Stack.Navigator>
+	      <Stack.Screen name="Home" component={Home} options={{ headerShown: false }} />
+			</Stack.Navigator>
+		</NavigationContainer>
   );
 }
 
